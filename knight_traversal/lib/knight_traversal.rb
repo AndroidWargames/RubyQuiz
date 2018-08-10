b4f6101af45f962f3ca325522d98e6b2b384d58d
Andrew Wagner <adwagner89@gmail.com>
2018-08-10 17:43:27 -0500

    fix references

--- knight_traversal/lib/knight_traversal.rb
+++ knight_traversal/lib/knight_traversal.rb
@@ -1,3 +1,8 @@
+require "knight_traversal/version"
+require "knight_traversal/knight_traversal_tree"
+require "knight_traversal/chess_square"
+require "knight_traversal/chess_board"
+
 module KnightTraversal
   class KnightCLI
     def self.get_path(start, finish, *bad_squares)
